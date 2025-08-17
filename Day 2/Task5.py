a343e7e660ecdafe92927f836f1d44637df3f4ea
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2025-08-17 23:08:46 +0500

    Created a Day 2 folder and I will create all the new tasks in it

--- Day 2/Task5.py
+++ Day 2/Task5.py
@@ -1,1 +1,13 @@
 # 🔹 Task 5: Word Reversal in Sentence
+
+sen = "I Love Python as it is"
+words = sen.split()
+rev_sen = []
+len_sen = len(words)
+print(len_sen)
+
+for char in range(len_sen - 1, -1, -1):
+    rev_sen.append(words[char])
+
+rev_sen_str = " ".join(rev_sen)
+print(rev_sen_str)
