6f0d0c21571226a9cc76ffa135e1755a4d7fed0b
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2025-08-24 11:51:26 +0500

    Created a week 2 folder and created day 8 folder in it.

---
+++ Week 2/Day 8/Task4.py
@@ -0,0 +1,12 @@
+# Find avg of numbers:
+# Without built-in Function:
+
+def avg(a, b):
+    return (a + b) / 2 
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another numbers: "))
+avg_num = avg(a, b)
+print(avg_num)
+
