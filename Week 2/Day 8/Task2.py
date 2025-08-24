52bd9b67db8723be8ceea7410ac2c9a9604a9092
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2025-08-24 11:43:35 +0500

    Created a week 2 folder and created day 8 folder in it and Task 2 file in it.

--- Week 2/Day 8/Task2.py
+++ Week 2/Day 8/Task2.py
@@ -1,1 +1,26 @@
 # Find minimums of numbers:
+
+# Without built-in Function:
+
+def min(a, b):
+    if a > b:
+        return b
+    else:
+        return a
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another numbers: "))
+min_num = min(a, b)
+print(min_num)
+
+
+# with Buit-in Function:
+
+def min_numb(a, b):
+    return min(a, b)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another numbers: "))
+min_num = min_numb(a, b)
+print(min_num)
