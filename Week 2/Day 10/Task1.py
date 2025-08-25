057f78ec835e6d5faf2548717ec2c245d6616923
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2025-08-25 12:43:49 +0500

    Created a week 2 folder and added all the days folder in it.

--- Week 2/Day 10/Task1.py
+++ Week 2/Day 10/Task1.py
@@ -1,2 +1,15 @@
 # Frequency counter using dict.
 
+def Freq_count(input):
+    dictionary = {}
+    for char in input:
+        if char in dictionary:
+            dictionary[char] += 1
+        else:
+            dictionary[char] = 1
+
+    return dictionary
+
+string = input("Enter text: ")
+output = Freq_count(string)
+print(output)
