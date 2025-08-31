7116319c4d8918e8b628b5ffb635e8faa340634a
Rana Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2025-08-31 15:08:51 +0500

    Created a week 3 folder and created day 15 folder in it.

--- Week 3/Day 15/Task.py
+++ Week 3/Day 15/Task.py
@@ -1,4 +1,4 @@
-# Linear & binary search
+# Linear search
 
 def linear_Search(arr, tar):
     for i in range(len(arr) - 1):
@@ -6,8 +6,31 @@ def linear_Search(arr, tar):
             return i
     return "Not found"
 
-numbers = input("Enter the array like this (1 2 3): ").split()
+numbers = input("Enter the array (space-separated): ").split()
 target = int(input("Enter the target value you want to find: "))
 array = [int(x) for x in numbers]
 search = linear_Search(array, target)
+print(f"Value found at index: {search}")
+
+
+# binary search
+
+def binary_search(arr, tar):
+    low, high = 0, len(arr)-1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == tar:
+            return mid
+        elif arr[mid] < tar:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return "Not found"
+
+
+numbers = input("Enter the array (space-separated): ").split()
+target = int(input("Enter the target value you want to find: "))
+array = sorted([int(x) for x in numbers])
+print(array)
+search = binary_search(array, target)
 print(f"Value found at index: {search}")
